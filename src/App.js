982c5b4347009e99727d9d457de0d4c9c4265976
Henri Pietila <henrismail@gmail.com>
2017-11-17 23:22:11 +0200

    adds sreact scroll buttons

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
   Segment,
   Visibility,
 } from 'semantic-ui-react'
+import ScrollableAnchor from 'react-scrollable-anchor'
 import logo from './draft1b.png'
 import pilers from './pilers.svg'
 import sunset from './sunset.svg'
@@ -88,87 +89,95 @@ export default class App extends Component {
                 inverted
                 style={{ fontSize: '1.7em', fontWeight: 'normal' }}
               />
-              <Button primary size='huge'>
+              <Button href='#section1' size='huge' color='purple'>
                 Get Started
                 <Icon name='right arrow' />
               </Button>
             </Container>
           </Segment>
         </Visibility>
+        <ScrollableAnchor id={'section1'}>
+          <div style={{
+            padding: '8em 0em',
+            background: '#f2f4f4'
+          }} >
+            <Segment style={{ padding: '2em 0em' }} vertical>
+              <Container text >
+                <Header as='h3' style={{ fontSize: '2em' }}>Fullstack web-development made easy</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  You provide the idea and we build it. It's that simple. </p>
+                <p>
+                  We handle all the heavy lifting: coordination, management, and talent. </p>
+                <p>
+                  Regardless of what stage you are at, greenfield or refreshing a legacy codebase, we have the expertise to handle it.
+                </p>
+              </Container>
+            </Segment>
+            <Segment style={{ padding: '0em' }} vertical>
+              <Grid celled='internally' columns='equal' stackable>
+                <Grid.Row textAlign='center'>
+                  <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                    <Header as='h3' style={{ fontSize: '2em' }}>San Francisco Quality at a fraction of the costs</Header>
+                    <p style={{ fontSize: '1.33em' }}>-No longer do you have pick between price, quality, or convince</p>
+                  </Grid.Column>
+                  <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                    <Header as='h3' style={{ fontSize: '2em' }}>"Professionally trained, expert engineers"</Header>
+                    <Header as='h3' style={{ fontSize: '2em' }}>We work with you every step of the way</Header>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Segment>
+            <Container textAlign='center'  style={{ padding: '2em' }} >
+            <Button href='#section3' size='huge' color='purple'>
+            Find Out More
+            <Icon name='right arrow' />
+          </Button>
+          </Container>
+          </div>
+        </ScrollableAnchor>
 
-        <div style={{
-          padding: '8em 0em',
-          background: '#f2f4f4'
-        }} >
-          <Segment style={{ padding: '2em 0em' }} vertical>
-            <Container text >
-              <Header as='h3' style={{ fontSize: '2em' }}>Fullstack web-development made easy</Header>
-              <p style={{ fontSize: '1.33em' }}>
-                You provide the idea and we build it. It's that simple. </p>
-              <p>
-                We handle all the heavy lifting: coordination, management, and talent. </p>
-              <p>
-                Regardless of what stage you are at, greenfield or refreshing a legacy codebase, we have the expertise to handle it.
-              </p>
-            </Container>
-          </Segment>
-          <Segment style={{ padding: '0em' }} vertical>
-            <Grid celled='internally' columns='equal' stackable>
-              <Grid.Row textAlign='center'>
-                <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                  <Header as='h3' style={{ fontSize: '2em' }}>San Francisco Quality at a fraction of the costs</Header>
-                  <p style={{ fontSize: '1.33em' }}>-No longer do you have pick between price, quality, or convince</p>
+        <ScrollableAnchor id={'section3'}>        
+          <Segment style={{
+            padding: '8em 0em',
+            backgroundImage: `url(${sunset})`,
+            minHeight: windowH * 1.2,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center center',
+            backgroundSize: 'cover'
+          }} vertical>
+            <Grid container stackable verticalAlign='middle'>
+              <Grid.Row>
+                <Grid.Column width={8}>
+                  <Header as='h3' style={{ fontSize: '2em' }}>Fast Turnaround and Communication</Header>
+                  <ul style={{ fontSize: '1.33em' }}>
+                    <li>Offices in Silicon Valley and Jordan</li>
+                    <li>We are always in your time zone</li>
+                    <li>Real-time and asynchronous communication</li>
+                  </ul>
+                  <Header as='h3' style={{ fontSize: '2em' }}>Agile Practices</Header>
+                  <ul style={{ fontSize: '1.33em' }}>
+                    <li>We help you plan sprints.</li>
+                    <li>Weekly big picture updates</li>
+                    <li>Daily updates on Git, Assna, and Slack</li>
+                  </ul>
                 </Grid.Column>
-                <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                  <Header as='h3' style={{ fontSize: '2em' }}>"Professionally trained, expert engineers"</Header>
-                  <Header as='h3' style={{ fontSize: '2em' }}>We work with you every step of the way</Header>
+                <Grid.Column floated='right' width={6}>
+                  <Image
+                    bordered
+                    rounded
+                    size='large'
+                    src={theatre}
+                  />
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column textAlign='center'>
+                  <Button size='huge' color='purple'> Ready?</Button>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
           </Segment>
-        </div>
-
-        <Segment style={{
-          padding: '8em 0em',
-          backgroundImage: `url(${sunset})`,
-          minHeight: windowH * 1.2,
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center center',
-          backgroundSize: 'cover'
-        }} vertical>
-          <Grid container stackable verticalAlign='middle'>
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Header as='h3' style={{ fontSize: '2em' }}>Fast Turnaround and Communication</Header>
-                <ul style={{ fontSize: '1.33em' }}>
-                  <li>Offices in Silicon Valley and Jordan</li>
-                  <li>We are always in your time zone</li>
-                  <li>Real-time and asynchronous communication</li>
-                </ul>
-                <Header as='h3' style={{ fontSize: '2em' }}>Agile Practices</Header>
-                <ul style={{ fontSize: '1.33em' }}>
-                  <li>We help you plan sprints.</li>
-                  <li>Weekly big picture updates</li>
-                  <li>Daily updates on Git, Assna, and Slack</li>
-                </ul>
-              </Grid.Column>
-              <Grid.Column floated='right' width={6}>
-                <Image
-                  bordered
-                  rounded
-                  size='large'
-                  src={theatre}
-                />
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column textAlign='center'>
-                <Button size='huge'>Get Started</Button>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
-
+        </ScrollableAnchor>
 
 
         <Segment inverted vertical style={{ padding: '5em 0em' }}>
